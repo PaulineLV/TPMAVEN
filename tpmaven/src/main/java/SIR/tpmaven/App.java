23f2618824fbb3150340ecaf9e778de3c0bf7016
CorinneB <corinne.bechepois@gmail.com>
2014-03-20 13:33:20 +0100

    tpmaven

---
+++ tpmaven/src/main/java/SIR/tpmaven/App.java
@@ -0,0 +1,15 @@
+package SIR.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+	/**
+	 * Hello world
+	 * @param args
+	 */
+    public static void main(final String[] args){
+    	System.out.println("Hello World!");
+   }
+}
